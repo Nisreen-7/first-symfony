51814567279d1ed3ae3660933f2259c1ff7183d1
nisreen <nisokh7@gmail.com>
2023-06-19 13:31:02 +0200

    commit test2

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Premier exo twig syntaxe de base
+
+
+	
+Dans le FirstController, créer une nouvelle Route sur "/exo-twig", créer également un nouveau template exo-twig.html.twig et en faire un render
+	
+Dans le render, ajouter une variable "names" à laquelle vous associerait un tableau de noms (par exemple ["Nom 1", "Nom 2", "Nom 3"] )
+	
+Côté template twig, utiliser cette variable names pour faire une boucle et afficher chaque nom dans un paragraphe
+	
+Rajouter un if dans le template pour faire en sorte d'afficher un message seulement si le tableau names contient plus de 3 noms
--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -1,17 +1,33 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FirstController extends AbstractController{
-#[Route("/test")]
-public function test(){
-    // return new Response("Bonjour");
-    return $this ->render("first.html.twig", [
-        "variable"=> "Form php",
-        "isOk"=>true
-    ]);
-}
+class FirstController extends AbstractController
+{
+    #[Route("/test")]
+    public function test()
+    {
+        // return new Response("Bonjour");
+        return $this->render("first.html.twig", [
+            "variable" => "Form php",
+            "isOk" => true
+        ]);
+    }
+
+
+    #[Route("/exo-twig")]
+    public function exoTwig()
+    {
+        $names = ["Nisreen", "Sara", "Louna", "Sameer"];
+        // return new Response("Bonjour");
+        return $this->render("exo-twig.html.twig", [
+            "names" => $names
+
+        ]);
+    }
+
 }
